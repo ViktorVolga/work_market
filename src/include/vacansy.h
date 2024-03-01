5b332965b6cbf639a0421d5d9f1b396a8d8aeaf1
Viktor <dorodnikovviktor@gmail.com>
2024-03-01 18:28:06 +0000

    work on Vacansy parser

--- src/include/vacansy.h
+++ src/include/vacansy.h
@@ -23,6 +23,16 @@ enum class Level
     unknown,
 };
 
+class Salary
+{
+    int from;
+    int to;
+public:
+    Salary(const int & from, const int & to);
+};
+
+typedef std::unique_ptr<Salary> salary_t;
+
 class Vacansy
 {
     int my_id                                {};
@@ -32,7 +42,7 @@ class Vacansy
     bool is_open                             {};
     std::string my_url                       {};
     std::string my_company                   {};
-    int my_rub_salary                        {};
+    salary_t my_salary                       {};
     Expirience my_expirience                 {};
     std::string my_description               {};
     std::vector<std::string> my_skills       {};
@@ -45,7 +55,7 @@ public:
     const std::string & get_my_name();
     void set_my_country(std::string &country);
     const std::string & get_my_country();
-    void set_my_sity(std::string &sity);
+    void set_my_sity(std::string sity);
     const std::string & get_my_sity();
     void set_is_open(bool open);
     bool get_is_open();
@@ -53,9 +63,9 @@ public:
     const std::string & get_my_url();
     void set_my_company(std::string & company);
     const std::string & get_my_company();
-    void set_my_salary(int & salary);
+    void set_my_salary(int min_salary, int max_salary);
     const int & get_my_rub_salary();
-    void set_my_expirience(Expirience & expirience);
+    void set_my_expirience(std::string expirience);
     const Expirience get_my_expirience();
     void set_my_description(std::string & description);
     const std::string & get_my_descripton(); 
--- src/vacansy.cpp
+++ src/vacansy.cpp
@@ -2,6 +2,12 @@
 #include "vacansy.h"
 #include "logger.h"
 
+Salary::Salary(const int & from, const int & to)
+{
+    this->from = from;
+    this->to = to;
+}
+
 Vacansy::Vacansy()
 {
 
@@ -39,11 +45,45 @@ const std::string & Vacansy::get_my_country()
     return my_country;
 }
 
-HHVacansy::HHVacansy(const json &vacansy_json)
+void Vacansy::set_my_sity(std::string sity)
+{
+    web_logger()->info("Vacansy::set_my_sity = [{}]", sity);
+    my_sity = sity;
+}
+
+void Vacansy::set_my_salary(int min_salary, int max_salary)
 {    
+    web_logger()->info("Vacansy::set_my_salary min = [{}], max = [{}]", min_salary, max_salary);   
+    my_salary = std::make_unique<Salary>(min_salary, max_salary);
+}
+
+void Vacansy::set_my_expirience(std::string expirience)
+{
+    web_logger()->info("Vacansy::set_my_expirience = [{}]",expirience);
+    if(expirience == "noExperience")
+        my_expirience =  Expirience::no_expirince;
+}
+
+HHVacansy::HHVacansy(const json &vacansy_json)
+{   
+    int min_salary, max_salary;  
     set_my_id(stoi(vacansy_json["id"].template get<std::string>()));
     set_my_name(vacansy_json["name"].template get<std::string>());
-
+    set_my_sity(vacansy_json["area"]["name"].template get<std::string>());
+    try{
+        min_salary = vacansy_json["salary"]["from"].template get<int>();
+    }
+    catch (const json::type_error& ex){
+        min_salary = 0;
+    }
+    try{
+        max_salary = vacansy_json["salary"]["to"].template get<int>();
+    }
+    catch (const json::type_error& ex){
+        max_salary = 0;
+    }
+    set_my_salary(min_salary, max_salary);
+    set_my_expirience(vacansy_json["experience"]["id"].template get<std::string>());
 }
 
 void HHVacansy::save()
